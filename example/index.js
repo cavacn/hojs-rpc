dce09b7ce758e3bce463afe93949e04c9108f15d
I'm EE <admin@cavacn.com>
2016-08-22 11:04:13 +0800

    增加err的status,message,stack返回结果

--- example/index.js
+++ example/index.js
@@ -5,28 +5,27 @@ const Hojs = require("hojs")
 
 const $ = new Hojs({
     path: __dirname,
-    engine: require("../lib")("myname","/rsos/mydemoservice")
+    engine: require("../lib")("myname", "/tuling-rsos/mydemoservice", {connect: "localhost:2181"})
 })
 
 $.api.get("/get/:aaaa")
-    .param("msg",{
-        type:"TrimString",
-        default:"没有参数",
-        coment:"消息内容"
+    .param("msg", {
+        type: "TrimString",
+        default: "没有参数",
+        coment: "消息内容"
     })
-    .param("aaaa",{
-        type:"TrimString"
+    .param("aaaa", {
+        type: "TrimString"
     })
     .required("msg")
-    .register(function(params,callback){
-        callback(null,params)
-        // callback(new Error("eeee"))
+    .register(function (params, callback) {
+        callback(null, params)
     })
 
-$.initAndListen("localhost",6668,function(err){
-    if(err){
+$.initAndListen("localhost", 6668, function (err) {
+    if (err) {
         process.exit(1)
-    }else{
+    } else {
         console.log("service start")
     }
 })
